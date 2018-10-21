e3fb0310c371ae31fe18b60ded322e5ec967e071
Nicolas Senaud <nicolas@senaud.fr>
2018-10-21 20:04:56 +0200

    Basic status post

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+#[macro_use]
+extern crate serde_json;
 extern crate my_internet_ip;
+extern crate reqwest;
 
 fn main() {
 	let ip: ::std::net::IpAddr = match my_internet_ip::get() {
@@ -7,4 +10,22 @@ fn main() {
 	};
 
 	println!("{}", ip);
+
+    let client = reqwest::Client::new();
+    let res: reqwest::Response = match client.post("https://slack.com/api/users.profile.set")
+        .bearer_auth("token")
+        .json(
+            &json!({
+                "profile": {
+                    "status_text": "test",
+                    "status_emoji": ":mountain_railway:",
+                    "status_expiration": 0
+                }
+            }))
+        .send() {
+            Ok(res) => res,
+            Err(e) => panic!("Failed to change status: {:?}", e),
+    };
+
+    println!("{:#?}", res);
 }
