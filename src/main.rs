9042c5315b266598fa42066011e9f4dabdf0b8ea
Nicolas Senaud <nicolas@senaud.fr>
2018-10-22 10:06:08 +0200

    Add Nation to possible locations

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,11 @@ use std::net::{IpAddr, Ipv4Addr};
 fn main() {
     let _home = IpAddr::V4(Ipv4Addr::new(176, 187, 98, 85));
     let _saint_martin = IpAddr::V4(Ipv4Addr::new(78, 193, 77, 48));
+    let _nation = IpAddr::V4(Ipv4Addr::new(81, 250, 187, 198));
 
     let ip: ::std::net::IpAddr = match my_internet_ip::get() {
         Ok(ip) => ip,
-        Err(e) => panic!("Could not get IP: {:?}", e)
+        Err(e) => panic!("Could not get IP: {:?}", e),
     };
 
     let status_text: &str;
@@ -20,6 +21,10 @@ fn main() {
     if ip == _saint_martin {
         println!("Saint-Martin");
         status_text = "ipso Saint-Martin";
+        status_emoji = ":ipso:";
+    } else if ip == _nation {
+        println!("Nation");
+        status_text = "ipso Nation";
         status_emoji = ":ipso:";
     } else if ip == _home {
         println!("Maison");
@@ -34,19 +39,19 @@ fn main() {
     println!("{}", ip);
 
     let client = reqwest::Client::new();
-    let res: reqwest::Response = match client.post("https://slack.com/api/users.profile.set")
+    let res: reqwest::Response = match client
+        .post("https://slack.com/api/users.profile.set")
         .bearer_auth("")
-        .json(
-            &json!({
+        .json(&json!({
                 "profile": {
                     "status_text": status_text,
                     "status_emoji": status_emoji,
                     "status_expiration": 0
                 }
-            }))
-        .send() {
-            Ok(res) => res,
-            Err(e) => panic!("Failed to change status: {:?}", e),
+            })).send()
+    {
+        Ok(res) => res,
+        Err(e) => panic!("Failed to change status: {:?}", e),
     };
 
     println!("{:#?}", res);
